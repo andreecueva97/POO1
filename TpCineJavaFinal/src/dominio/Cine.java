0e0d25c210e0432967610a869933896370fac1c9
andreecueva97 <andreecueva6@gmail.com>
2020-06-29 01:17:27 -0300

    Revert "TpCineJavaFInal2018"
    This reverts commit 99020ef52fc610b00208bd4cf2c565405aee69aa.

--- TpCineJavaFinal/src/dominio/Cine.java
+++
@@ -1,7 +0,0 @@
-package dominio;
-
-import java.util.List;
-
-public class Cine {
-	private List <Sala> Salas;
-}
--- TpCineJavaFinal/src/dominio/Funcion.java
+++
@@ -1,6 +0,0 @@
-package dominio;
-
-public class Funcion {
-	private int horaio;
-	private int entradasVendidas;
-}
